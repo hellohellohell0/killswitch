eb655694bcbb1e0fc19b03bacf9992ae4e6a22f5
hellohellohell0 <hhellohellohell012322@gmail.com>
2025-09-02 01:55:40 -0700

    Update kill.js

--- api/kill.js
+++ api/kill.js
@@ -1,5 +1,5 @@
 // /api/kill.js
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).send("none"); // your redirect URL
+  res.status(200).send("https://google.com"); // your redirect URL
 }
