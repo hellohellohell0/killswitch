9bcaef0d50151f8850ec670170c4e0882da50802
hellohellohell0 <hhellohellohell012322@gmail.com>
2025-09-02 01:48:12 -0700

    Update kill.js

--- api/kill.js
+++ api/kill.js
@@ -1,28 +1,6 @@
-// server.js
-const express = require("express");
-const app = express();
-const port = 3000;
+let redirectUrl = "none"; // default state
 
-// default state
-let redirectUrl = "https://google.com";
-
-// endpoint the clients will poll
-app.get("/killswitch", (req, res) => {
-  res.send(redirectUrl);
-});
-
-// endpoint for you (admin) to set the redirect
-app.get("/set", (req, res) => {
-  const url = req.query.url;
-  if (url) {
-    redirectUrl = url;
-    res.send(`Killswitch activated. Redirecting users to: ${url}`);
-  } else {
-    redirectUrl = "none";
-    res.send("Killswitch deactivated. Returning 'none'.");
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Killswitch server running at http://localhost:${port}`);
-});
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow frontend to call
+  res.status(200).send(redirectUrl);
+}
